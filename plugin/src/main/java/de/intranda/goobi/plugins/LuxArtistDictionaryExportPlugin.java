8c79ef6c72ca8055cf99a6b962bd1a143457fbb4
Robert Sehr <robert.sehr@intranda.com>
2021-10-29 15:52:12 +0200

    export data, create bibliography

--- plugin/src/main/java/de/intranda/goobi/plugins/LuxArtistDictionaryExportPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/LuxArtistDictionaryExportPlugin.java
@@ -10,10 +10,6 @@ import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.interfaces.IExportPlugin;
 import org.goobi.production.plugin.interfaces.IPlugin;
 
-import de.sub.goobi.config.ConfigPlugins;
-import de.sub.goobi.export.dms.ExportDms;
-import de.sub.goobi.helper.Helper;
-import de.sub.goobi.helper.VariableReplacer;
 import de.sub.goobi.helper.exceptions.DAOException;
 import de.sub.goobi.helper.exceptions.ExportFileException;
 import de.sub.goobi.helper.exceptions.SwapException;
@@ -23,7 +19,11 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 import ugh.dl.DigitalDocument;
+import ugh.dl.DocStruct;
 import ugh.dl.Fileformat;
+import ugh.dl.Metadata;
+import ugh.dl.MetadataGroup;
+import ugh.dl.MetadataType;
 import ugh.dl.Prefs;
 import ugh.exceptions.DocStructHasNoTypeException;
 import ugh.exceptions.MetadataTypeNotAllowedException;
@@ -31,6 +31,7 @@ import ugh.exceptions.PreferencesException;
 import ugh.exceptions.ReadException;
 import ugh.exceptions.TypeNotAllowedForParentException;
 import ugh.exceptions.WriteException;
+import ugh.fileformats.mets.MetsModsImportExport;
 
 @PluginImplementation
 @Log4j2
@@ -69,35 +70,94 @@ public class LuxArtistDictionaryExportPlugin implements IExportPlugin, IPlugin {
     SwapException, DAOException, TypeNotAllowedForParentException {
         problems = new ArrayList<>();
 
-        // read information from config file
-        String test = ConfigPlugins.getPluginConfig(title).getString("value", "");		
-        System.out.println("value from configuration file: " + test);
-        
-        // read mets file to test if it is readable
+
         try {
-        	Prefs prefs = process.getRegelsatz().getPreferences();
-        	Fileformat ff = null;
+            // read mets file
+            Prefs prefs = process.getRegelsatz().getPreferences();
+            MetadataType published = prefs.getMetadataTypeByName("Published");
+
+            Fileformat ff = null;
             ff = process.readMetadataFile();
             DigitalDocument dd = ff.getDigitalDocument();
-            VariableReplacer replacer = new VariableReplacer(dd, prefs, process, null);
+
+            // check if record should be exported
+            DocStruct logical = dd.getLogicalDocStruct();
+            List<? extends Metadata> md= logical.getAllMetadataByType(published);
+            if (md.isEmpty()) {
+                // TODO set message to skip
+                return true;
+            }
+            if ("N".equalsIgnoreCase(md.get(0).getValue())) {
+                // TODO set message to skip
+                return true;
+            }
+            // otherwise record is marked as exportable
+
+            List<MetadataGroup> allSources = new ArrayList<>();
+            List<MetadataGroup> bibliographyList = new ArrayList<>();
+            // run through all groups, check if group should be exported
+            for (MetadataGroup grp : logical.getAllMetadataGroups()) {
+                for (Metadata metadata : grp.getMetadataList()) {
+                    if (metadata.getType().getName().equals("Published") && metadata.getValue().equalsIgnoreCase("N")) {
+                        // TODO remove group, if its marked as not exportable
+                    }
+                }
+                // collect all sources
+                List<MetadataGroup> sources = grp.getAllMetadataGroupsByName("Source");
+                allSources.addAll(sources);
+                if (grp.getType().getName().equals("Bibliography")) {
+                    bibliographyList.add(grp);
+                }
+            }
+
+
+
+            // create bibliography from exported sources
+            for (MetadataGroup currentSource : allSources) {
+                boolean sourceMatched = false;
+                String sourceId = currentSource.getMetadataByType("SourceID").get(0).getValue();
+                for (MetadataGroup bibliography : bibliographyList) {
+                    // TODO extent bibliography to save vocab id (SourceID)
+                    if (bibliography.getMetadataByType("Link").get(0).getValue().equals(sourceId)) {
+                        sourceMatched = true;
+                        break;
+                    }
+                }
+                if (!sourceMatched) {
+                    MetadataGroup bib = new MetadataGroup(prefs.getMetadataGroupTypeByName("Bibliography"));
+                    Metadata type = new Metadata(prefs.getMetadataTypeByName("Type"));
+                    type.setValue(currentSource.getMetadataByType("SourceType").get(0).getValue());
+                    bib.addMetadata(type);
+
+
+                    Metadata citation = new Metadata(prefs.getMetadataTypeByName("Citation"));
+                    citation.setValue(currentSource.getMetadataByType("SourceName").get(0).getValue());
+                    bib.addMetadata(citation);
+
+                    Metadata link = new Metadata(prefs.getMetadataTypeByName("Link"));
+                    link.setValue(currentSource.getMetadataByType("SourceLink").get(0).getValue());
+                    bib.addMetadata(link);
+
+
+
+                    logical.addMetadataGroup(bib);
+                    bibliographyList.add(bib);
+                }
+            }
+
+            //TODO extend metadata with vocabulary information ?
+
+            // export data
+            MetsModsImportExport mm = new MetsModsImportExport(prefs);
+            mm.setDigitalDocument(dd);
+            mm.write(destination + process.getTitel()+ ".xml");
         } catch (ReadException | PreferencesException | WriteException | IOException | InterruptedException | SwapException | DAOException e) {
             log.error(e);
             problems.add("Cannot read metadata file.");
             return false;
         }
-        
-        // do a regular export here
-        IExportPlugin export = new ExportDms();
-        export.setExportFulltext(true);
-        export.setExportImages(true);
 
-        // execute the export and check the success
-        boolean success = export.startExport(process);
-        if (!success) {
-            log.error("Export aborted for process with ID " + process.getId());
-        } else {
-            log.info("Export executed for process with ID " + process.getId());
-        }        
-        return success;
+
+        return true;
     }
 }
--- plugin/src/test/java/de/intranda/goobi/plugins/LuxArtistDictionaryExportPluginTest.java
+++ plugin/src/test/java/de/intranda/goobi/plugins/LuxArtistDictionaryExportPluginTest.java
@@ -1,68 +1,178 @@
-package de.intranda.goobi.plugins;
-
-import static org.junit.Assert.assertNotNull;
-
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.XMLConfiguration;
-import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
-import org.easymock.EasyMock;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.junit.runner.RunWith;
-import org.powermock.api.easymock.PowerMock;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-
-import de.sub.goobi.config.ConfigPlugins;
-
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ ConfigPlugins.class })
-@PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*" })
-public class LuxArtistDictionaryExportPluginTest {
-
-    @Rule
-    public TemporaryFolder folder = new TemporaryFolder();
-    private File tempFolder;
-    private String resourcesFolder;
-
-    @Before
-    public void setUp() throws Exception {
-        tempFolder = folder.newFolder("tmp");
-
-        resourcesFolder = "src/test/resources/"; // for junit tests in eclipse
-
-        if (!Files.exists(Paths.get(resourcesFolder))) {
-            resourcesFolder = "target/test-classes/"; // to run mvn test from cli or in jenkins
-        }
-
-        PowerMock.mockStatic(ConfigPlugins.class);
-        EasyMock.expect(ConfigPlugins.getPluginConfig(EasyMock.anyString())).andReturn(getConfig()).anyTimes();
-        PowerMock.replay(ConfigPlugins.class);
-    }
-
-    @Test
-    public void testConstructor() {
-        LuxArtistDictionaryExportPlugin plugin = new LuxArtistDictionaryExportPlugin();
-        assertNotNull(plugin);
-    }
-
-    private XMLConfiguration getConfig() {
-        String file = "plugin_intranda_export_sample.xml";
-        XMLConfiguration config = new XMLConfiguration();
-        config.setDelimiterParsingDisabled(true);
-        try {
-            config.load(resourcesFolder + file);
-        } catch (ConfigurationException e) {
-        }
-        config.setReloadingStrategy(new FileChangedReloadingStrategy());
-        return config;
-    }
-
-}
+//package de.intranda.goobi.plugins;
+//
+//import static org.junit.Assert.assertNotNull;
+//
+//import java.io.File;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//
+//import org.apache.commons.configuration.ConfigurationException;
+//import org.apache.commons.configuration.XMLConfiguration;
+//import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+//import org.easymock.EasyMock;
+//import org.goobi.beans.Process;
+//import org.goobi.beans.Project;
+//import org.goobi.beans.Ruleset;
+//import org.junit.Before;
+//import org.junit.Rule;
+//import org.junit.Test;
+//import org.junit.rules.TemporaryFolder;
+//import org.junit.runner.RunWith;
+//import org.powermock.api.easymock.PowerMock;
+//import org.powermock.core.classloader.annotations.PowerMockIgnore;
+//import org.powermock.core.classloader.annotations.PrepareForTest;
+//import org.powermock.modules.junit4.PowerMockRunner;
+//
+//import de.sub.goobi.config.ConfigPlugins;
+//import de.sub.goobi.config.ConfigurationHelper;
+//import de.sub.goobi.helper.CloseStepHelper;
+//import de.sub.goobi.helper.VariableReplacer;
+//import de.sub.goobi.metadaten.MetadatenHelper;
+//import de.sub.goobi.persistence.managers.ProcessManager;
+//import de.sub.goobi.persistence.managers.StepManager;
+//import ugh.dl.Fileformat;
+//import ugh.dl.Prefs;
+//import ugh.fileformats.mets.MetsMods;
+//
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest({ MetadatenHelper.class, VariableReplacer.class, ConfigPlugins.class, StepManager.class, ConfigurationHelper.class,
+//    ProcessManager.class, CloseStepHelper.class })
+//@PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*", "jdk.internal.reflect.*", "com.sun.org.apache.xerces.*",
+//    "javax.xml.*", "org.xml.*" })
+//public class LuxArtistDictionaryExportPluginTest {
+//
+//    @Rule
+//    public TemporaryFolder folder = new TemporaryFolder();
+//    private File processDirectory;
+//    private File metadataDirectory;
+//    private Process process;
+//
+//    private String resourcesFolder;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//
+//        resourcesFolder = "src/test/resources/"; // for junit tests in eclipse
+//        if (!Files.exists(Paths.get(resourcesFolder))) {
+//            resourcesFolder = "target/test-classes/"; // to run mvn test from cli or in jenkins
+//        }
+//
+//        metadataDirectory = folder.newFolder("metadata");
+//
+//        processDirectory = new File(metadataDirectory + File.separator + "1");
+//        processDirectory.mkdirs();
+//        String metadataDirectoryName = metadataDirectory.getAbsolutePath() + File.separator;
+//
+//        // copy meta.xml
+//        Path metaSource = Paths.get(resourcesFolder + "meta.xml");
+//        Path metaTarget = Paths.get(processDirectory.getAbsolutePath(), "meta.xml");
+//        Files.copy(metaSource, metaTarget);
+//
+//        // copy ruleset
+//        XMLConfiguration config = getConfig();
+//        PowerMock.mockStatic(ConfigPlugins.class);
+//        EasyMock.expect(ConfigPlugins.getPluginConfig("intranda_workflow_projectexport")).andReturn(config).anyTimes();
+//
+//        PowerMock.mockStatic(ConfigurationHelper.class);
+//        ConfigurationHelper configurationHelper = EasyMock.createMock(ConfigurationHelper.class);
+//        EasyMock.expect(ConfigurationHelper.getInstance()).andReturn(configurationHelper).anyTimes();
+//        EasyMock.expect(configurationHelper.getMetsEditorLockingTime()).andReturn(1800000l).anyTimes();
+//        EasyMock.expect(configurationHelper.isAllowWhitespacesInFolder()).andReturn(false).anyTimes();
+//        EasyMock.expect(configurationHelper.useS3()).andReturn(false).anyTimes();
+//        EasyMock.expect(configurationHelper.isUseMasterDirectory()).andReturn(true).anyTimes();
+//        EasyMock.expect(configurationHelper.getRulesetFolder()).andReturn(resourcesFolder).anyTimes();
+//
+//        EasyMock.expect(configurationHelper.isCreateMasterDirectory()).andReturn(false).anyTimes();
+//        EasyMock.expect(configurationHelper.isCreateSourceFolder()).andReturn(false).anyTimes();
+//        EasyMock.expect(configurationHelper.getProcessImagesMainDirectoryName()).andReturn("RM0166F05-0000001_media").anyTimes();
+//        EasyMock.expect(configurationHelper.getFolderForInternalProcesslogFiles()).andReturn("intern").anyTimes();
+//        EasyMock.expect(configurationHelper.getMetadataFolder()).andReturn(metadataDirectoryName).anyTimes();
+//
+//        EasyMock.expect(configurationHelper.getScriptCreateDirMeta()).andReturn("").anyTimes();
+//        EasyMock.replay(configurationHelper);
+//        PowerMock.mockStatic(VariableReplacer.class);
+//        EasyMock.expect(VariableReplacer.simpleReplace(EasyMock.anyString(), EasyMock.anyObject())).andReturn("RM0166F05-0000001_media").anyTimes();
+//        PowerMock.replay(VariableReplacer.class);
+//
+//        PowerMock.mockStatic(CloseStepHelper.class);
+//        //        EasyMock.expect(CloseStepHelper.closeStep(EasyMock.anyObject(), EasyMock.anyObject())).andAnswer(new IAnswer<Boolean>() {
+//        //
+//        //            @Override
+//        //            public Boolean answer() throws Throwable {
+//        //                for (Step step : process.getSchritte()) {
+//        //                    if (step.getTitel().equals("test step to close")) {
+//        //                        step.setBearbeitungsstatusEnum(StepStatus.DONE);
+//        //                    } else if (step.getTitel().equals("locked step that should open")) {
+//        //                        step.setBearbeitungsstatusEnum(StepStatus.OPEN);
+//        //                    }
+//        //                }
+//        //
+//        //                return true;
+//        //            }
+//        //        }).anyTimes();
+//        //
+//        //        PowerMock.replay(CloseStepHelper.class);
+//
+//        Prefs prefs = new Prefs();
+//        prefs.loadPrefs(resourcesFolder + "ruleset.xml");
+//        Fileformat ff = new MetsMods(prefs);
+//        ff.read(metaTarget.toString());
+//
+//        PowerMock.mockStatic(MetadatenHelper.class);
+//        EasyMock.expect(MetadatenHelper.getMetaFileType(EasyMock.anyString())).andReturn("mets").anyTimes();
+//        EasyMock.expect(MetadatenHelper.getFileformatByName(EasyMock.anyString(), EasyMock.anyObject())).andReturn(ff).anyTimes();
+//
+//        PowerMock.mockStatic(StepManager.class);
+//        PowerMock.mockStatic(ProcessManager.class);
+//        PowerMock.replay(ConfigPlugins.class);
+//        PowerMock.replay(ConfigurationHelper.class);
+//        PowerMock.replay(MetadatenHelper.class);
+//        process = getProcess();
+//        EasyMock.expect(ProcessManager.getProcessById(EasyMock.anyInt())).andReturn(process).anyTimes();
+//        PowerMock.replay(ProcessManager.class);
+//    }
+//
+//    @Test
+//    public void testConstructor() throws Exception {
+//        LuxArtistDictionaryExportPlugin plugin = new LuxArtistDictionaryExportPlugin();
+//        assertNotNull(plugin);
+//        plugin.startExport(process);
+//    }
+//
+//    private XMLConfiguration getConfig() {
+//        String file = "plugin_intranda_export_sample.xml";
+//        XMLConfiguration config = new XMLConfiguration();
+//        config.setDelimiterParsingDisabled(true);
+//        try {
+//            config.load(resourcesFolder + file);
+//        } catch (ConfigurationException e) {
+//        }
+//        config.setReloadingStrategy(new FileChangedReloadingStrategy());
+//        return config;
+//    }
+//
+//    public Process getProcess() {
+//        Project project = createProject();
+//
+//        Process process = new Process();
+//        process.setTitel("RM0166F01-0000001");
+//        process.setProjekt(project);
+//        process.setId(1);
+//
+//        Ruleset ruleset = new Ruleset();
+//        process.setRegelsatz(ruleset);
+//        ruleset.setDatei("ruleset.xml");
+//
+//        return process;
+//    }
+//
+//    private Project createProject() {
+//        Project project = new Project();
+//        project.setTitel("SampleProject");
+//        project.setMetsRightsOwner("Centro Bibliografico");
+//        project.setMetsRightsOwnerSite("http://ucei.it/centro-bibliografico/");
+//        return project;
+//    }
+//
+//}
